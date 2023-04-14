bb6d577d91b50a602dc0fdfe15fcc2d6721fe5f4
Indilaforever <130746967+Indilaforever@users.noreply.github.com>
2023-04-14 11:00:42 +0200

    Add files via upload

---
+++ python projet.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 09:55:28 2023
+
+@author: mathi
+"""
+import random
+
+def niveasy(x):
+    k= random.randint(4)
+    if k==1:
+      condition=(couleur,'=',nombre) 
+      return(condition)
+    elif k==2:
+       condition=(couleur,'<',3) 
+       return(condition)
+    elif k==3:
+        condition=(couleur,'<',5)
+        return(condition)
+    elif k==4:
+        condition=(couleur,'>'7)
+        return(conditon)
